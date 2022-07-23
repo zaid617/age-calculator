de14f171472c022085b8870da529d25c3546e2d1
Zaid_ZiD <zid6177@gmail.com>
2022-07-23 15:10:10 +0500

    update app.js

--- app.js
+++ app.js
@@ -13,59 +13,71 @@ function calculate() {
     let age = todayYear - yearOfDOB;
     let days = document.getElementById('days');
     let ages = document.getElementById('age');
-    let dob2 = document.getElementById('dob2');
+
     let nextBirthyear;
 
-    if (todayMonth > monthOfDOB) {
-        nextBirthyear = todayYear + 1
-        let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime();
-        let daysLeft = Math.floor((nextBirthyearTime - todayTime) / (1000 * 60 * 60 * 24));
-        days.value = daysLeft;
-        ages.value = age;
-        dob2.value = dateOfDOB + "/" + (monthOfDOB+1) + "/" + yearOfDOB;
-        console.log(daysLeft, " days are left");
-    }
-    else if (todayMonth < monthOfDOB) {
-        nextBirthyear = todayYear
-        let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime()
-        let daysLeft = Math.floor((nextBirthyearTime - todayTime) / (1000 * 60 * 60 * 24));
-        days.value = daysLeft;
-        ages.value = age
-        dob2.value = dateOfDOB + "/" + (monthOfDOB+1) + "/" + yearOfDOB;
-        console.log("your birthday passed :", daysLeft + "day ago");
-    }
-    else if (todayMonth == monthOfDOB) {
+    {
+        if (todayMonth == monthOfDOB) {
 
-        if (todayDate > dateOfDOB) {
-            nextBirthyear = todayYear + 1
-            let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime();
-            let daysLeft = Math.floor((nextBirthyearTime - todayTime) / (1000 * 60 * 60 * 24));
-            days.value = daysLeft;
-            ages.value = age
-            dob2.value = dateOfDOB + "/" +  (monthOfDOB+1) + "/" +  yearOfDOB;
-            console.log(daysLeft, " days are left");
+            if (todayDate > dateOfDOB) {
+                nextBirthyear = todayYear + 1
+                let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime();
+                let daysLeft = Math.floor((nextBirthyearTime - todayTime) / (1000 * 60 * 60 * 24));
+                days.value = daysLeft;
+                ages.value = age
+                console.log("Birthday", dob);
+                console.log("age", age);
+                console.log(daysLeft, " days are left");
+                return
+            }
+
+            else if (todayDate < dateOfDOB) {
+                nextBirthyear = todayYear
+                let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime()
+                let daysLeft = Math.floor(( nextBirthyearTime - todayTime) / (1000 * 60 * 60 * 24));
+                console.log("days left:", daysLeft );
+                days.value = daysLeft;
+                ages.value = age
+                return
+
+            }
+            else if (todayDate === dateOfDOB) {
+                let daysLeft = 00
+                days.value = daysLeft;
+                ages.value = age
+                console.log(daysLeft);
+                return
+            }
         }
+        {
+            if (todayMonth < monthOfDOB) {
+                nextBirthyear = todayYear
+                let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime()
+                let daysLeft = Math.floor((nextBirthyearTime - todayTime) / (1000 * 60 * 60 * 24));
+                days.value = daysLeft;
+                ages.value = age;
+                console.log("Birthday", dob);
+                console.log("age", age);
+                console.log("days left", daysLeft);
+                return
+            }
+        } {
+            if (todayMonth > monthOfDOB) {
+                nextBirthyear = todayYear + 1
+                let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime();
+                let daysLeft = Math.floor((nextBirthyearTime - todayTime) / (1000 * 60 * 60 * 24));
+                days.value = daysLeft;
+                ages.value = age;
+                console.log("Birthday", dob);
+                console.log("age", age);
+                console.log(daysLeft, " days are left");
+                return
+            }
 
-        else if (todayDate < dateOfDOB) {
-            nextBirthyear = todayYear
-            let nextBirthyearTime = new Date(nextBirthyear, monthOfDOB, dateOfDOB).getTime()
-            let daysLeft = Math.floor((todayTime - nextBirthyearTime) / (1000 * 60 * 60 * 24));
-            console.log("your birthday passed :", daysLeft + "day ago");
-            days.value = daysLeft;
-            ages.value = age
-            dob2.value = dateOfDOB + "/" +  (monthOfDOB+1) + "/" +  yearOfDOB;
         }
-        else if (todayDate === dateOfDOB) {
-            let daysLeft = "Its Your Birthday"
-            days.value = daysLeft;
-            ages.value = age
-            dob2.value = dateOfDOB + "/" +  (monthOfDOB+1) + "/" +  yearOfDOB;
-            console.log(daysLeft);
-        }
-
     }
 
-    
+
 
 }
 
